49b2048444ccf9f69de11ca52e7df05a9428bd8d
Ivan Avvakumov <avvakumov@protonmail.com>
2020-05-11 10:19:36 +0300

    Fix radix tree to properly work

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@ mod trie;
 mod radix;
 
 use trie::TrieNode;
+use radix::*;
+use std::error::Error;
 
 fn main() {
-    let mut root: TrieNode<String> = TrieNode::new();
-    for i in ["test", "this", "hashmap", "test1", "trie"].iter() {
-        println!("{}", i);
-        root.add(&mut i.chars().map(|s| {s.to_string()}));
+    let mut radix_tree = radix::RadixTree::<String>::new(); 
+    let vec = [("test", "test"), ("this", "this"), ("hashmap", "hashmap"), ("test1", "test1"), ("trie", "trie"), ("test12", "test12")];
+    for i in vec.iter() {
+        println!("{:?}", i);
+        radix_tree.insert(i.0.to_string(), i.1.to_string());
     }
-    println!("{}", root.find(&mut "hashap".chars().map(|s| {s.to_string()})));
+    for i in vec.iter() {
+        assert_eq!(i.0, radix_tree.find(i.0.to_string()).unwrap());
+    }
+    radix_tree.print_edges();
+    println!("{}", radix_tree.find("this".to_string()).unwrap());
 }
--- src/radix.rs
+++ src/radix.rs
@@ -1,22 +1,24 @@
 use std::rc::Rc;
 use std::ops::{Index,IndexMut};
+use std::collections::VecDeque;
 
-struct Edge<T> {
-    target_node: Rc<Box<Node<T>>>,
+
+struct Edge {
+    target_node: usize,
     label: String
 }
 
-impl<T> Edge<T> {
-    fn new(target_node: Node<T>, label: String) -> Self {
+impl Edge {
+    fn new(target_node: usize, label: String) -> Self {
         Self {
-            target_node: Rc::new(Box::new(target_node)),
+            target_node, 
             label
         }
     }
 }
 
 struct Node<T> {
-    edges: Vec<Rc<Box<Edge<T>>>>,
+    edges: Vec<usize>,
     value: T,
     is_leaf: bool
 }
@@ -74,13 +76,20 @@ impl<T> Arena<T> {
         }
     }
 
-    fn insert(&mut self, val: T) {
+    fn insert(&mut self, val: T) -> usize {
         if self.arr_idx.is_empty() {
             self.arr.push(val);
+            return self.arr.len() - 1;
         } else {
             self.arr[self.arr_idx[0]] = val;
+            let idx = self.arr_idx[0];
             self.arr_idx.remove(0);
+            return idx;
         }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.arr.is_empty()
     }
 
     fn delete(&mut self, idx: usize) {
@@ -89,39 +98,159 @@ impl<T> Arena<T> {
     }
 }
 
-fn lookup<T>(mut search_node: Rc<Box<Node<T>>>, key: String) -> (Ans, Rc<Box<Node<T>>>) {
-    let mut count = 0;
-    while !search_node.is_leaf && count < key.len() {
-        for e in &search_node.edges {
-            if e.label.starts_with(&key[count..]) {
-                count += e.label.len();
-                search_node = Rc::clone(&e.target_node);
-                break
+pub struct RadixTree<T> {
+    nodes: Arena<Node<T>>,
+    edges: Arena<Edge>
+}
+
+impl<T: std::default::Default + std::fmt::Debug> RadixTree<T> {
+    pub fn new() -> Self {
+        let mut radix_tree = Self {
+            nodes: Arena::<Node<T>>::new(),
+            edges: Arena::<Edge>::new()
+        };
+        let zero_node_idx = radix_tree.nodes.insert(Node::new(T::default()));
+        radix_tree.edges.insert(Edge::new(zero_node_idx, "".to_string()));
+        radix_tree
+    }
+
+    fn common_prefix(&self, first_str: &str, second_str: &str) -> Option<String> {
+        let matching = first_str.chars().zip(second_str.chars()).take_while(|&(a, b)| a == b).count();
+        if matching > 0 {
+            return Some(first_str[..matching].to_string());
+        }
+        None
+    }
+
+    fn lookup(&self, key: String) -> (Ans, usize) {
+        let mut idx = 0;
+        let mut edge_idx = 0;
+        let mut count = 0;
+        let mut found = true;
+        let mut node = &self.nodes[idx];
+        while found && !node.is_leaf && count < key.len() {
+            found = false;
+            for &e_idx in &node.edges {
+                let edge = &self.edges[e_idx];
+                // if the label have a prefix of a suffix of the key
+                // example: looking for the word "testing" when we already
+                // have "test", "tests", "testing"
+                //      "test"
+                //      /    \
+                //   "s"     "ing"
+                if key[count..].starts_with(&edge.label) {
+                    count += edge.label.len();
+                    idx = e_idx;
+                    found = true;
+                    break;
+                } else {
+                    // in a case when a label might be longer we need to consider to split the node
+                    // if there is a common prefix > 0
+                    if let Some(cp) = self.common_prefix(&key[count..], &edge.label) {
+                        println!("Key: {}, label: {}", &key[count..], &edge.label);
+                        count += cp.len() + key.len();
+                        idx = e_idx;
+                        break;
+                    }
+                }
+            }
+            if found {
+                node = &self.nodes[self.edges[idx].target_node];
             }
         }
+        // if exact same key was found
+        println!("{}, {}, {}, {}, {}, {}", idx, self.edges[idx].target_node, count, key.len(),
+        self.nodes[self.edges[idx].target_node].is_leaf, found);
+        if count == key.len() {
+            return (Ans{exists:true, count}, idx);
+        }
+        (Ans{exists: false, count}, idx)
     }
-    if search_node.is_leaf && count == key.len() {
-        return (Ans{exists:true, count}, search_node);
-    }
-    (Ans{exists: false, count}, search_node)
-}
 
-fn find<T: Clone>(search_node: Rc<Box<Node<T>>>, key: String) -> Option<T> {
-    if let (ans, node) = lookup(search_node, key) {
+    pub fn find(&self, key: String) -> Option<&T> {
+        let (ans, idx) = self.lookup(key);
         if ans.exists {
-            return Some(node.value.clone());
+            return Some(&self.nodes[self.edges[idx].target_node].value);
+        }
+        None
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.edges.is_empty()
+    }
+
+    pub fn print_nodes(&self) {
+        let mut q = VecDeque::new();
+        q.push_front(0);
+        while !q.is_empty() {
+            let mut level_size = q.len();
+            while level_size > 0 {
+                let n = q.pop_front().unwrap();
+                print!("{:#?}   ", self.nodes[n].value);
+                for &edge in &self.nodes[n].edges {
+                    q.push_back(self.edges[edge].target_node);
+                }
+                level_size -= 1;
+            }
+            println!();
         }
     }
-    None
-}
 
-fn insert<T>(root: Rc<Box<Node<T>>>, key: String, val: T) {
-    if let (ans, node) = lookup(root, key) {
+    pub fn print_edges(&self) {
+        let mut q = VecDeque::new();
+        q.push_front(0);
+        while !q.is_empty() {
+            let mut level_size = q.len();
+            while level_size > 0 {
+                let n = q.pop_front().unwrap();
+                print!("{:#?}   ", self.edges[n].label);
+                for &edge in &self.nodes[self.edges[n].target_node].edges {
+                    q.push_back(edge);
+                }
+                level_size -= 1;
+            }
+            println!();
+        }
+    }
+
+    pub fn insert(&mut self, key: String, val: T) {
+        if self.is_empty() {
+            let new_node = Node::new(val);
+            let new_node_idx = self.nodes.insert(new_node);
+            let new_edge = Edge::new(new_node_idx, key);
+            let new_edge_idx = self.edges.insert(new_edge);
+            self.nodes[0].edges.push(new_edge_idx);
+            self.nodes[0].is_leaf = false;
+            return
+        }
+        let (ans, idx) = self.lookup(key.clone());
+        let target_node_idx = self.edges[idx].target_node;
         if !ans.exists {
-            if ans.count < key.len() {
+            if ans.count <= key.len() {
+                // case when we have to add new node with suffix
+                let node_idx = self.nodes.insert(Node::new(val));
+                let edge_idx = self.edges.insert(Edge::new(node_idx,key[ans.count..].to_string()));
+                self.nodes[target_node_idx].edges.push(edge_idx);
+                self.nodes[target_node_idx].is_leaf = false;
+            } else {
+                // case when we have to split node using common prefix
+                //let split_node = self.nodes[target_node_idx].clone();
+                let mut split_node = Node::new(T::default());
+                split_node.is_leaf = false;
+                let label = self.edges[idx].label.clone();
+                let count = ans.count - key.len();
+                
+                self.edges[idx].label = key[..count].to_string();
+                let edge_left = Edge::new(self.edges[idx].target_node, label[count..].to_string());
+                let edge_left_idx = self.edges.insert(edge_left);
                 let new_node = Node::new(val);
-                let new_edge = Edge::new(new_node, key[ans.count..].to_string());
-                node.edges.push(Rc::new(Box::new(new_edge)));
+                let new_node_idx = self.nodes.insert(new_node);
+                let edge_right = Edge::new(new_node_idx, key[count..].to_string());
+                let edge_right_idx = self.edges.insert(edge_right);
+                split_node.edges.push(edge_left_idx);
+                split_node.edges.push(edge_right_idx);
+                let split_node_idx = self.nodes.insert(split_node);
+                self.edges[idx].target_node = split_node_idx;
             }
         }
     }
